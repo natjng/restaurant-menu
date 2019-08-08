9f12283f2b7dab0bf9f1f6ded1cbe8f733a9a714
Natalie <natsetc@gmail.com>
2019-08-08 15:17:56 -0700

    rename username to name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < ApplicationController
     def home
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
+        # if session[:user_id]
+        #     @user = User.find_by(id: session[:user_id])
+        # end
     end
 
     def create 
         @user = User.find_or_create_by(id: auth['uid']) do |u|
-            u.username = auth['info']['name']
+            u.name = auth['info']['name']
             u.email = auth['info']['email']
         end
         session[:user_id] = @user.id
-        render :home
+        render 'users/show'
     end
 
     private
