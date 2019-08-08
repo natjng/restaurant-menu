e0fab0ce6d0c27711e249373304a0ee83fd3bcdb
Natalie <natsetc@gmail.com>
2019-08-08 14:17:56 -0700

    add #create and #auth for omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,21 @@
 class SessionsController < ApplicationController
     def home
-        
+
     end
 
     def create 
-
+        @user = User.find_or_create_by(id: auth['uid']) do |u|
+            u.username = auth['info']['name']
+            u.email = auth['info']['email']
+        end
+        sessions[:user_id] = @user.id
+        render :home
     end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
+
 end
