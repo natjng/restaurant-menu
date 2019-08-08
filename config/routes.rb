ec1aa01a0f0bca8d62f65d1d0ec1bd15e235eeaf
Natalie <natsetc@gmail.com>
2019-08-08 16:58:36 -0700

    create login and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'sessions#home'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   
   get '/auth/facebook/callback', to: 'sessions#create'
