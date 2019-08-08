bfa2488f790ca3935b0663dbffb531350a0a66fe
Natalie <natsetc@gmail.com>
2019-08-08 15:16:42 -0700

    rename username to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_101325) do
+ActiveRecord::Schema.define(version: 2019_08_08_221457) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2019_08_07_101325) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
