0bd0d649955dfced278ef0545c4cd748316ecc2f
Natalie <natsetc@gmail.com>
2019-08-08 16:13:47 -0700

    create login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'sessions#home'
+  get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   
   get '/auth/facebook/callback', to: 'sessions#create'
