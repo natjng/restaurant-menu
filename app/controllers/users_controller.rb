20e62cf5840d58a763d4149c47c14ed59543130f
Natalie <natsetc@gmail.com>
2019-08-08 14:58:44 -0700

    add before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :destroy]
+
+
+    private
+    
+    def set_user
+        @user = current_user
+    end
 end
