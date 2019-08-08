d4b1a915396da6ac6ed4fb94dac4821fdd9012a0
Natalie <natsetc@gmail.com>
2019-08-08 15:21:52 -0700

    add #destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:show, :destroy]
 
 
+
+    def destroy
+        session.destroy
+        redirect_to '/'
+    end
+
     private
     
     def set_user
