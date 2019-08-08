8bdae3a5bc110d3e39de8f7ce8dc5cd4e5d2ae5e
Natalie <natsetc@gmail.com>
2019-08-08 16:55:55 -0700

    enable login for app created users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,21 @@ class SessionsController < ApplicationController
     end
 
     def new
-        @user = User.new
     end
 
     def create 
-        @user = User.find_or_create_by(id: auth['uid']) do |u|
-            u.name = auth['info']['name']
-            u.email = auth['info']['email']
+        @user = User.find_by_email(params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+        # logging in w/omniauth fb
+        # @user = User.find_or_create_by(id: auth['uid']) do |u|
+        #     u.name = auth['info']['name']
+        #     u.email = auth['info']['email']
+        # end
+            session[:user_id] = @user.id
+            render 'users/show'
+        else
+            render :new
         end
-        session[:user_id] = @user.id
-        render 'users/show'
     end
 
     def destroy
