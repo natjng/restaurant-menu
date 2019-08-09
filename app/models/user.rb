83b4a6105989803846896879724fe3bb114dcb64
Natalie <natsetc@gmail.com>
2019-08-08 17:08:36 -0700

    add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+
     has_many :restaurants
     has_secure_password
 end
