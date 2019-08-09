a53e1645c800aa3a89409c6fed6ac36debefe602
Natalie <natsetc@gmail.com>
2019-08-08 17:49:41 -0700

    remove unnecessary info from #home

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,5 @@
 class SessionsController < ApplicationController
     def home
-        # if session[:user_id]
-        #     @user = User.find_by(id: session[:user_id])
-        # end
     end
 
     def new
