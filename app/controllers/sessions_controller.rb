82cd563df4ba1a2c5a848959d177b6e41d915f28
Natalie <natsetc@gmail.com>
2019-08-08 20:17:00 -0700

    allow app user and omniauth user to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,18 @@ class SessionsController < ApplicationController
     end
 
     def create 
-        @user = User.find_by_email(params[:user][:email])
-        if @user && @user.authenticate(params[:user][:password])
-        # logging in w/omniauth fb
-        # @user = User.find_or_create_by(id: auth['uid']) do |u|
-        #     u.name = auth['info']['name']
-        #     u.email = auth['info']['email']
-        # end
+        if auth
+            @user = User.find_or_create_by_omniauth(auth)
             session[:user_id] = @user.id
             render 'users/show'
         else
-            render :new
+            @user = User.find_by_email(params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                render 'users/show'
+            else
+                render :new
+            end
         end
     end
 
