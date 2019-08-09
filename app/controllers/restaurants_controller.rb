ccd780408da1b351a5a5ccba3ff2d4b66053b795
Natalie <natsetc@gmail.com>
2019-08-09 14:46:38 -0700

    create restaurants new and edit actions and views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,12 @@ class RestaurantsController < ApplicationController
     def show
         @restaurant = Restaurant.find_by(id: params[:id])
     end
+
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    def edit
+        @restaurant = Restaurant.find_by(id: params[:id])
+    end
 end
