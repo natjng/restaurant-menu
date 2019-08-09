659b31836d06231f5dc5b8b5cf69068143deefd6
Natalie <natsetc@gmail.com>
2019-08-09 15:09:55 -0700

    add set_menu before_action

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,12 @@
 class MenusController < ApplicationController
+    before_action :set_menu, only: [:show, :edit, :update]
+
     def show
+    end
+
+    private
+
+    def set_menu
         @menu = Menu.find_by(id: params[:id])
     end
 end
