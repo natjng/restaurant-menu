b5ea45dd880e2fc7f0d41270455d2a478a15b227
Natalie <natsetc@gmail.com>
2019-08-09 15:01:30 -0700

    restaurant create action and params private method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,7 +7,22 @@ class RestaurantsController < ApplicationController
         @restaurant = Restaurant.new
     end
 
+    def create
+        @restaurant = current_user.restaurants.build(restaurant_params)
+        if @restaurant.save
+            redirect_to @restaurant
+        else
+            render :new
+        end
+    end
+
     def edit
         @restaurant = Restaurant.find_by(id: params[:id])
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit!
     end
 end
