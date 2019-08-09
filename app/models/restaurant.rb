0c155e8def75e392eb3103ce6b2a46aa01bbf944
Natalie <natsetc@gmail.com>
2019-08-09 03:07:13 -0700

    edit phone number validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
-  validates :phone_number, numericality: true, length: { maximum: 15 }
-  # accoounts for international phone numbers
+  validates :phone_number, length: {maximum: 10}
 
   belongs_to :user
   has_many :menus
