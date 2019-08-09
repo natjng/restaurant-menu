5a8812285f575ad5260a50fa1c627d449b53ad9f
Natalie <natsetc@gmail.com>
2019-08-09 03:04:58 -0700

    allow nil for price attr

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   validates :name, presence: true
-  validates :price, numericality: true
+  validates :price, numericality: true, allow_nil: true
 
   belongs_to :menu
   belongs_to :category
