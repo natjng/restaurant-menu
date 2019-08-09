93f6bb65db3dbc06e77adfa9a1f10de88fc99a65
Natalie <natsetc@gmail.com>
2019-08-08 17:18:30 -0700

    add restaurant validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :phone_number, length: { maximum: 15 }
+  # accoounts for international phone numbers
+
   belongs_to :user
   has_many :menus
 end
