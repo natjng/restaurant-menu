c690771275b1721803fa45cbc8521dfd4e0926d8
Natalie <natsetc@gmail.com>
2019-08-08 20:16:29 -0700

    create #find_or_create_by_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
 
-    has_many :restaurants
     has_secure_password
+    has_many :restaurants
+    
+
+    def self.find_or_create_by_omniauth(auth)
+        self.find_or_create_by(email: auth['info']['email']) do |u|
+            u.name = auth['info']['name']
+            u.password = SecureRandom.hex
+        end
+    end
+
 end
