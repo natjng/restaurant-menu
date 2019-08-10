862c1965b35d3fad540ddb1b86cfdf988bf597d6
Natalie <natsetc@gmail.com>
2019-08-09 17:05:07 -0700

    create set_category before_action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,12 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :edit, :update]
+
     def show
+    end
+
+    private
+
+    def set_category
         @category = Category.find_by(id: params[:id])
     end
 end
