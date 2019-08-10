3b363136c918251512fa912376001e73e2068e76
Natalie <natsetc@gmail.com>
2019-08-09 17:13:02 -0700

    create categories new action and view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
     before_action :set_category, only: [:show, :edit, :update]
 
-    def show
+    def new
+        @category = Category.new
     end
 
     private
