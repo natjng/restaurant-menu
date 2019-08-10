94c4ae147197983b5cb49220ae7052f43125267f
Natalie <natsetc@gmail.com>
2019-08-09 17:32:18 -0700

    set_item before_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
+    before_action :set_item, only: [:show, :edit, :update]
+
     def show
+    end
+
+    private
+
+    def set_item
         @item = Item.find_by(id: params[:id])
     end
+
 end
