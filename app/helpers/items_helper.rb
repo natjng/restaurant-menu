d0e20adecbb34bcc0df36cd4777ea04e9d0fa9da
Natalie <natsetc@gmail.com>
2019-08-10 20:26:16 -0700

    moved helper to menu help

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,9 +1,2 @@
 module ItemsHelper
-    def menu_id_field(menu)
-        if @item.menu.nil?
-            select_tag "item[menu_id]", options_from_collection_for_select(Menu.all, :id, :name)
-        else
-            hidden_field_tag "item[menu_id]", item.menu_id
-        end
-    end
 end
