b6d4bf1815f6e6a1de50136ba3ca8c0e3334c1ab
Natalie <natsetc@gmail.com>
2019-08-11 13:30:31 -0700

    update phone number validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,20 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
-  validates :phone_number, length: {maximum: 10}
+  validates :phone_number, length: {is: 10}, numericality: true, allow_nil: true
 
   belongs_to :user
   has_many :menus
+
+  scope :address_like, -> { where("address LIKE ?", "%ca%") }
+
+  def self.restaurant_menu_categories
+    Restaurant.all.map do |r|
+      r.menus.each do |menu|
+        menu.categories
+      end
+    end
+  end
+
+  # restaurants, menus, categories
+
 end
