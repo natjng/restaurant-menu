612f2ad952d680d39f67a471d3440441e3ffcfec
Natalie <natsetc@gmail.com>
2019-08-10 20:27:24 -0700

    add menu helper

--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -1,2 +1,9 @@
 module MenusHelper
+    def menu_select(menu, item)
+        if menu
+            menu.name
+        else
+            select_tag "item[menu_id]", options_from_collection_for_select(Menu.all, :id, :name)
+        end
+    end
 end
