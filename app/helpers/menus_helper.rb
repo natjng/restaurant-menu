c5ad526dd98712b3b9e5f7d910408311cd694236
Natalie <natsetc@gmail.com>
2019-08-12 01:48:57 -0700

    use helpers to show users menus and categories

--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -3,7 +3,7 @@ module MenusHelper
         if menu
             hidden_field_tag "item[menu_id]", item.menu_id
         else
-            select_tag "item[menu_id]", options_from_collection_for_select(Menu.all, :id, :name)
+            select_tag "item[menu_id]", options_from_collection_for_select(User.all_menus(current_user), :id, :name)
         end
     end
 end
