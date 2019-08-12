6f404a18e4baf384bd333b6196f0d72d63442788
Natalie <natsetc@gmail.com>
2019-08-11 17:16:06 -0700

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,14 @@ Rails.application.routes.draw do
   get '/auth/facebook/callback', to: 'sessions#create'
 
   get '/items/no_price', to: 'items#no_price'
-  get '/restaurants/address_like', to: 'restaurants#address_like'
   
-  resources :items
-  resources :categories
-  resources :menus
-  resources :restaurants
-  resources :users, except: [:index, :destroy]
-
   resources :menus do
     resources :items, only: [:index, :show, :new, :edit]
-    resources :categories, only: [:index, :show, :new, :edit]
   end
+  resources :items
+  resources :categories, except: :destroy
+  resources :restaurants
+  resources :users, except: [:index, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
