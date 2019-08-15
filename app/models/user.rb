e244abbb0827cca9a1b576f80ab588cf45b0b3a5
Natalie <natsetc@gmail.com>
2019-08-14 19:01:00 -0700

    add nested associations, remove extra methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
 
     has_secure_password
     has_many :restaurants
+    has_many :menus, through: :restaurants
+    has_many :items, through: :menus
+    has_many :categories, through: :items
     
 
     def self.find_or_create_by_omniauth(auth)
@@ -11,44 +14,6 @@ class User < ApplicationRecord
             u.name = auth['info']['name']
             u.password = SecureRandom.hex
         end
-    end
-
-    def self.all_items(current_user)
-        user_items = []
-        current_user.restaurants.each do |r|
-            r.menus.each do |menu|
-                menu.items.each do |item|
-                    user_items << item
-                end
-            end
-        end
-        user_items
-    end
-
-    def self.all_categories(current_user)
-        user_categories = []
-        current_user.restaurants.each do |r|
-            r.menus.each do |menu|
-                menu.categories.each do |category|
-                    user_categories << category
-                end
-            end
-        end
-        user_categories.uniq
-    end
-
-    def self.all_menus(current_user)
-        user_menus = []
-        current_user.restaurants.each do |r|
-            r.menus.each do |menu|
-                user_menus << menu
-            end
-        end
-        user_menus
-    end
-
-    def self.menu_ids(current_user)
-        User.all_menus(current_user).map{|menu| menu.id}
     end
 
 end
